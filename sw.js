3003ea59046540c0534ed6fe9e6cca1a0ea606d2
rakweb <rakweb@users.noreply.github.com>
2026-03-13 17:27:35 -0500

    Update sw.js v2

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
 const CACHE = 'health-tracker-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./index.html'])).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(['./index.html'])).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
