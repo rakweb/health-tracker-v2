1be8438a92f37ccfe5e1b6158172cba3eb16d21b
rakweb <rakweb@users.noreply.github.com>
2026-05-16 08:06:19 -0500

    Refactor validation logic into Validator object
    Refactored validation logic into a Validator object with an entry method. Removed old validateEntry function and added error checks for date and glucose.

--- js/validator.js
+++ js/validator.js
@@ -1,16 +1,12 @@
-export function validateEntry(e) {
-  const errors = [];
+// js/validator.js
+const Validator = {
+    entry(data) {
+        const errors = [];
+        if (!data.date) errors.push("Date is required");
+        if (data.glucose && (data.glucose < 0 || data.glucose > 1000)) 
+            errors.push("Glucose must be between 0-1000");
+        return { valid: errors.length === 0, errors };
+    }
+};
 
-  if (!e.date) errors.push('Date is required');
-
-  if (e.sys && (e.sys < 70 || e.sys > 250))
-    errors.push('Systolic out of safe range');
-
-  if (e.dia && (e.dia < 40 || e.dia > 150))
-    errors.push('Diastolic out of safe range');
-
-  if (e.hr && (e.hr < 30 || e.hr > 220))
-    errors.push('Heart rate out of safe range');
-
-  return errors;
-}
+window.Validator = Validator;
